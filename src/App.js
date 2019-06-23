a0a02a68acd38b0db349825676fe785f89f41605
koy <kosatun@aol.com>
2019-06-23 14:38:20 -0700

    created components ImageLinkForm, Rank, Logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from "react";
 import Navigation from "./components/navigation/Navigation";
+import Logo from "./components/logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
 
 import "./App.css";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particleOptions} />
       <Navigation />
-      {/* <Logo />
+      <Logo />
+      <Rank />
       <ImageLinkForm />
-      <FaceRecognition /> */}
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <div>
-      <p>Sign Out</p>
-    </div>
+    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+    </nav>
   );
 };
 
